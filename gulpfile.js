8dc4ed4904cbbfef19d71ec55f0c0cd4bc1626cd
Atoche Pascual <jmatochepascual@gmail.com>
2023-04-12 04:15:15 -1000

    [ADD] soporte para navegadores

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 const { src, dest, watch } = require('gulp'); // npm i -D gulp
 const sass = require('gulp-sass')(require('sass')); // npm i -D sass gulp-sass
+const postcss = require('gulp-postcss'); // npm i -D gulp-postcss
+const autoprefixer = require('autoprefixer'); // npm i -D autoprefixer
 
 
 function css(done) {
   src('./src/scss/app.scss')
-    .pipe(sass())
+    .pipe(sass({ outputStyle: 'compressed' })) // expanded para debug and  compressed para minificar 
+    .pipe(postcss([autoprefixer()]))
     .pipe(dest('./build/css'))
   done();
 };
