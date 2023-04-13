5c7fb111755dc649cada502641be79214cb284b8
Atoche Pascual <jmatochepascual@gmail.com>
2023-04-13 05:37:00 -1000

    [ADD] soporte para optimizacion de imagenes webp avif

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const sass = require('gulp-sass')(require('sass')); // npm i -D sass gulp-sass
 const postcss = require('gulp-postcss'); // npm i -D gulp-postcss
 const autoprefixer = require('autoprefixer'); // npm i -D autoprefixer
 
+const imagemin = require('gulp-imagemin'); // npm i -D gulp-imagemin@7.1.0
+const webp = require('gulp-webp'); // npm i -D gulp-webp
+const avif = require('gulp-avif'); // npm i -D gulp-avif
+
 
 function css(done) {
   src('./src/scss/app.scss')
@@ -15,9 +19,47 @@ function css(done) {
 
 function dev() {
   watch('./src/scss/**/*.scss', css);
+  watch('./src/scss/**/', imagenes);
+};
+
+
+function imagenes(done) {
+  src('./src/img/**/*')
+    .pipe(imagemin({ optimizationLevel: 5 }))
+    .pipe(dest('./build/img'))
+  done();
+};
+
+
+function versionWebp(done) {
+  const opciones = {
+    quality: 50
+  }
+
+  src('./src/img/**/*.{jpg,png}')
+    .pipe(webp(opciones))
+    .pipe(dest('./build/img'))
+  done();
+};
+
+
+function versionAvif(done) {
+  const opciones = {
+    quality: 50
+  }
+
+  src('./src/img/**/*.{jpg,png}')
+    .pipe(avif(opciones))
+    .pipe(dest('./build/img'))
+  done();
 };
 
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css, dev);
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
+
+// npm i -D gulp sass gulp-sass gulp-postcss autoprefixer gulp-imagemin@7.1.0 gulp-webp gulp-avif
